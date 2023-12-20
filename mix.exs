b4cbfc3c49f4ecf79adf19fe0be4867a58635093
alejandro.oliva <alejandro.oliva@bluetab.net>
2023-12-20 13:07:52 +0000

    TD-6274 Version 5.20.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdQx.MixProject do
       app: :td_qx,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "5.17.0-local"
+          nil -> "5.20.0-local"
           v -> v
         end,
       elixir: "~> 1.14",
