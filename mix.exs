fa2340ba470814f21edf211933415f95c288b40d
Alejandro Oliva López <alejandro.oliva@bluetab.net>
2024-06-17 15:35:57 +0000

    TD-6499 update td-df-lib new content format

--- mix.exs
+++ mix.exs
@@ -60,7 +60,7 @@ defmodule TdQx.MixProject do
       {:bodyguard, "~> 2.4"},
       {:quantum, "~> 3.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:td_core, git: "https://github.com/Bluetab/td-core.git", tag: "6.8.0"},
+      {:td_core, git: "https://github.com/Bluetab/td-core.git", tag: "6.8.1"},
       {:cors_plug, "~> 3.0"},
       {:ex_machina, "~> 2.7.0", only: :test},
       {:mox, "~> 1.0", only: :test}
